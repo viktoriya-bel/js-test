e355056d19553849b603b5e855c2070926716407
vb@finfort.ru <vb@finfort.ru>
2020-04-30 18:28:04 +0400

    профессии

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -73,7 +73,7 @@ function tablePeopleRowBuild(
 	nameLast,
 	date,
 	thumbnail,
-	profession = "Безработный"
+	profession = [{ title: "Безработный" }]
 ) {
 	let tablePeople = ""
 	const pencilIcon =
@@ -95,13 +95,9 @@ function tablePeopleRowBuild(
 		pencilIcon +
 		'</th><th><img src="' +
 		thumbnail +
-		'"/></th><th>'
+		'"/></th><th class="js-profession-column">'
 	tablePeople +=
-		profession +
-		'<svg class="bi bi-gear js-profession-setting" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">\n' +
-		'  <path fill-rule="evenodd" d="M8.837 1.626c-.246-.835-1.428-.835-1.674 0l-.094.319A1.873 1.873 0 014.377 3.06l-.292-.16c-.764-.415-1.6.42-1.184 1.185l.159.292a1.873 1.873 0 01-1.115 2.692l-.319.094c-.835.246-.835 1.428 0 1.674l.319.094a1.873 1.873 0 011.115 2.693l-.16.291c-.415.764.42 1.6 1.185 1.184l.292-.159a1.873 1.873 0 012.692 1.116l.094.318c.246.835 1.428.835 1.674 0l.094-.319a1.873 1.873 0 012.693-1.115l.291.16c.764.415 1.6-.42 1.184-1.185l-.159-.291a1.873 1.873 0 011.116-2.693l.318-.094c.835-.246.835-1.428 0-1.674l-.319-.094a1.873 1.873 0 01-1.115-2.692l.16-.292c.415-.764-.42-1.6-1.185-1.184l-.291.159A1.873 1.873 0 018.93 1.945l-.094-.319zm-2.633-.283c.527-1.79 3.065-1.79 3.592 0l.094.319a.873.873 0 001.255.52l.292-.16c1.64-.892 3.434.901 2.54 2.541l-.159.292a.873.873 0 00.52 1.255l.319.094c1.79.527 1.79 3.065 0 3.592l-.319.094a.873.873 0 00-.52 1.255l.16.292c.893 1.64-.902 3.434-2.541 2.54l-.292-.159a.873.873 0 00-1.255.52l-.094.319c-.527 1.79-3.065 1.79-3.592 0l-.094-.319a.873.873 0 00-1.255-.52l-.292.16c-1.64.893-3.433-.902-2.54-2.541l.159-.292a.873.873 0 00-.52-1.255l-.319-.094c-1.79-.527-1.79-3.065 0-3.592l.319-.094a.873.873 0 00.52-1.255l-.16-.292c-.892-1.64.902-3.433 2.541-2.54l.292.159a.873.873 0 001.255-.52l.094-.319z" clip-rule="evenodd"/>\n' +
-		'  <path fill-rule="evenodd" d="M8 5.754a2.246 2.246 0 100 4.492 2.246 2.246 0 000-4.492zM4.754 8a3.246 3.246 0 116.492 0 3.246 3.246 0 01-6.492 0z" clip-rule="evenodd"/>\n' +
-		"</svg>" +
+		professionUpdateTable(profession) +
 		"</th>" +
 		'<th><i class="icon-eye-open" title="Посмотреть обязанности"></i></th>' +
 		'<th><svg class="bi bi-trash js-removePeople" title="Удалить" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">\n' +
@@ -189,10 +185,12 @@ $(".js-table-people").on("click", ".js-editPeople", function() {
 
 // eslint-disable-next-line require-jsdoc
 function RemoveAll(element, arr, index) {
+	arr = Array.prototype.slice.call(arr)
 	arr.splice(index, 1)
 	const parentElements = element.parentElement
 	element.remove()
 	for (index; index < arr.length; index++) {
+		console.log(parentElements.children[index].firstElementChild)
 		parentElements.children[index].firstElementChild.innerHTML = index
 	}
 }
@@ -249,20 +247,24 @@ $(".js-ModalAdd").submit(function() {
 
 const ProfessionArr = [
 	{
-		title: "Программист",
-		charge: { 0: "Писать код", 1: "Отрастить бороду" }
-	},
-	{
-		title: "Дизайнер",
-		charge: { 0: "Рисовать", 1: "Придумывать" }
+		profession: [
+			{
+				title: "Программист",
+				charge: { 0: "Писать код", 1: "Отрастить бороду" }
+			},
+			{
+				title: "Дизайнер",
+				charge: { 0: "Рисовать", 1: "Придумывать" }
+			}
+		]
 	}
 ]
 
 $(".js-ModalProfession-show").on("click", function() {
 	// eslint-disable-next-line new-cap
-	ProfessionView(ProfessionArr)
+	ProfessionView(ProfessionArr[0].profession)
 	const professionBlockAdd =
-		'<input class="form-control js-profession-people-adds" type="text" placeholder="Введите название профессии">'
+		'<input class="form-control js-profession-input-adds" type="text" placeholder="Введите название профессии">'
 	document.getElementsByClassName(
 		"js-profession-adds"
 	)[0].innerHTML = professionBlockAdd
@@ -270,14 +272,20 @@ $(".js-ModalProfession-show").on("click", function() {
 		' <button class="btn  btn-primary js-profession-add-btn" type="button"> Добавить профессию </button>'
 })
 $(".js-ModalProfession").on("click", ".js-profession-add-btn", function() {
-	const val = document.getElementsByClassName("js-profession-people-adds")[0]
+	const val = document.getElementsByClassName("js-profession-input-adds")[0]
 		.value
 	// const length = Object.keys(obj[number].profession).length
 	console.log(ProfessionArr)
 	// eslint-disable-next-line new-cap
 	ProfessionAddSave(ProfessionArr, val, 0)
-	// // eslint-disable-next-line new-cap
-	// ProfessionView(ProfessionArr[this.dataset.num].profession)
+	console.log(ProfessionArr)
+	// eslint-disable-next-line new-cap
+	ProfessionView(ProfessionArr[0].profession)
+	document.getElementsByClassName("js-profession-input-adds")[0].value = ""
+})
+$("#ModalProfession").on("show.bs.modal", function(e) {
+	const errorText = document.getElementsByClassName("js-error-exists")[0]
+	errorText.classList.add("hide")
 })
 // eslint-disable-next-line require-jsdoc
 function ProfessionView(arr) {
@@ -294,7 +302,12 @@ function ProfessionView(arr) {
 				profession += '<li>' + obj[key].charge[key2] + '<i class="icon-pencil" title="Изменить обязанности"></i><i class="icon-remove"  title="Удалить"></i></li>';
 			}*/
 			profession +=
-				'</ul><i class="icon-plus" title="Добавить обязанность"></i></th><th><i class="icon-remove" title="Удалить"></i></th></tr>'
+				'</ul><i class="icon-plus" title="Добавить обязанность"></i></th><th><i class="icon-remove" title="Удалить"></i></th>'
+			profession +=
+				'<th><svg class="bi bi-trash js-removeProfession" title="Удалить" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">\n' +
+				'  <path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z"/>\n' +
+				'  <path fill-rule="evenodd" d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" clip-rule="evenodd"/>\n' +
+				"</svg></th></tr>"
 		})
 	}
 	document.getElementsByClassName(
@@ -305,12 +318,13 @@ function ProfessionView(arr) {
 $(".js-table-people").on("click", ".js-profession-setting", function() {
 	const numEl = this.parentElement.parentElement.firstElementChild
 	$(".js-ModalProfession").modal("show")
+	// console.log(PeopleArray[numEl.textContent].profession)
 	// eslint-disable-next-line new-cap
-	ProfessionView(PeopleArray.profession)
+	ProfessionView(PeopleArray[numEl.textContent].profession)
 	let professionBlockAdd =
 		'<div class="dropdown mr-1">\n' +
 		'    <select class="form-control js-profession-people-adds" id="exampleFormControlSelect1">\n'
-	ProfessionArr.forEach(function(item, i, arr) {
+	ProfessionArr[0].profession.forEach(function(item, i, arr) {
 		professionBlockAdd += "      <option>" + item.title + "</option>\n"
 	})
 	professionBlockAdd += "    </select>\n" + "  </div>"
@@ -335,13 +349,43 @@ $(".js-ModalProfession").on(
 		ProfessionView(PeopleArray[this.dataset.num].profession)
 	}
 )
+$(".js-ModalProfession").on("click", ".js-removeProfession", function() {
+	// eslint-disable-next-line new-cap
+	// ProfessionDelete(PeopleArray, this)
+	// eslint-disable-next-line new-cap
+	const el = document.getElementsByClassName("js-profession-add-people-btn")[0]
+	console.log(el)
+	console.log(PeopleArray[el.dataset.num].profession)
+	console.log(this.parentElement.parentElement)
+	console.log(Number(this.parentElement.parentElement.firstElementChild.innerHTML))
+	RemoveAll(
+		this.parentElement.parentElement,
+		PeopleArray[el.dataset.num].profession,
+		Number(this.parentElement.parentElement.firstElementChild.innerHTML)
+	)
+	// eslint-disable-next-line new-cap
+	// ProfessionView(PeopleArray[this.dataset.num].profession)
+})
+
+// eslint-disable-next-line require-jsdoc
+function professionUpdateTable(arr) {
+	let html = ""
+	Object.keys(arr).map(function(key, index) {
+		html += arr[key].title + "</br>"
+	})
+	html +=
+		'<svg class="bi bi-gear js-profession-setting" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">\n' +
+		'  <path fill-rule="evenodd" d="M8.837 1.626c-.246-.835-1.428-.835-1.674 0l-.094.319A1.873 1.873 0 014.377 3.06l-.292-.16c-.764-.415-1.6.42-1.184 1.185l.159.292a1.873 1.873 0 01-1.115 2.692l-.319.094c-.835.246-.835 1.428 0 1.674l.319.094a1.873 1.873 0 011.115 2.693l-.16.291c-.415.764.42 1.6 1.185 1.184l.292-.159a1.873 1.873 0 012.692 1.116l.094.318c.246.835 1.428.835 1.674 0l.094-.319a1.873 1.873 0 012.693-1.115l.291.16c.764.415 1.6-.42 1.184-1.185l-.159-.291a1.873 1.873 0 011.116-2.693l.318-.094c.835-.246.835-1.428 0-1.674l-.319-.094a1.873 1.873 0 01-1.115-2.692l.16-.292c.415-.764-.42-1.6-1.185-1.184l-.291.159A1.873 1.873 0 018.93 1.945l-.094-.319zm-2.633-.283c.527-1.79 3.065-1.79 3.592 0l.094.319a.873.873 0 001.255.52l.292-.16c1.64-.892 3.434.901 2.54 2.541l-.159.292a.873.873 0 00.52 1.255l.319.094c1.79.527 1.79 3.065 0 3.592l-.319.094a.873.873 0 00-.52 1.255l.16.292c.893 1.64-.902 3.434-2.541 2.54l-.292-.159a.873.873 0 00-1.255.52l-.094.319c-.527 1.79-3.065 1.79-3.592 0l-.094-.319a.873.873 0 00-1.255-.52l-.292.16c-1.64.893-3.433-.902-2.54-2.541l.159-.292a.873.873 0 00-.52-1.255l-.319-.094c-1.79-.527-1.79-3.065 0-3.592l.319-.094a.873.873 0 00.52-1.255l-.16-.292c-.892-1.64.902-3.433 2.541-2.54l.292.159a.873.873 0 001.255-.52l.094-.319z" clip-rule="evenodd"/>\n' +
+		'  <path fill-rule="evenodd" d="M8 5.754a2.246 2.246 0 100 4.492 2.246 2.246 0 000-4.492zM4.754 8a3.246 3.246 0 116.492 0 3.246 3.246 0 01-6.492 0z" clip-rule="evenodd"/>\n' +
+		"</svg>"
+	return html
+}
 
 // eslint-disable-next-line require-jsdoc
 function ProfessionAddSave(obj, val, number) {
 	const errorText = document.getElementsByClassName("js-error-exists")[0]
 	// eslint-disable-next-line new-cap
 	if (ProfessionСheck(obj[number].profession, val) === true) {
-		console.log("ProfessionСheck")
 		errorText.classList.remove("hide")
 	} else {
 		errorText.classList.add("hide")
@@ -357,9 +401,25 @@ function ProfessionAddSave(obj, val, number) {
 			obj[number].profession[length] = {
 				title: val
 			}
-			console.log("ProfessionСheck2")
 		}
+		const el = document.getElementsByClassName("js-table-people")[0]
+		el.childNodes[number].querySelector(
+			".js-profession-column"
+		).innerHTML = professionUpdateTable(obj[number].profession, number)
 	}
+}
+// eslint-disable-next-line require-jsdoc
+function ProfessionDelete(obj, el) {
+	console.log(obj)
+	console.log(el)
+	// const length = Object.keys(obj[number].profession).length
+	// obj[number].profession[length] = {
+	// 	title: val
+	// }
+	// const el = document.getElementsByClassName("js-table-people")[0]
+	// el.childNodes[number].querySelector(
+	// 	".js-profession-column"
+	// ).innerHTML = professionUpdateTable(obj[number].profession, number)
 }
 
 // eslint-disable-next-line require-jsdoc
